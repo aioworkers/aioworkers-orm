e631268023c1304bcb0e915d1f6bfed769baf2c4
Alexander Bogushov <abogushov@gmail.com>
2019-04-05 17:09:59 +0500

    Refactoring

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,13 +1,14 @@
-async def test_orm(context):
+async def test_context_models(context):
     assert context.models.test
-    await context.models.test.objects.create(id=1)
+    o = await context.models.test.objects.create(id=1)
+    assert o
     o = await context.models.test.get_by_id(1)
     assert o
     o = await context.models.test.get_by_id_sql(1)
     assert o
 
 
-async def test_model_class(context):
+async def test_orm(context):
     from .models import Test
     d = await Test.objects.all()
     assert d
